b00f363c59222e3c0bc40843cfdd07b8a8721323
willythor <willem.thorbecke@students.olin.edu>
2018-02-26 12:07:39 -0500

    matcher function

--- src/matcher.c
+++ src/matcher.c
@@ -3,7 +3,17 @@
 
 
 int matcher(char *input) {
-  if (strcmp(input, "quit")) {
+  if (strcmp(input, "quit") == 0) {
     return quit;
   }
+
+  if (strncmp(input, "cd", 2) == 0) {
+  	return cd;
+  }
+
+  if (strncmp(input, "ls", 2) == 0) {
+  	return ls;
+  }
+
+  return 1;
 }
