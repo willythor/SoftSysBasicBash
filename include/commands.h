58397632211a1e8516ca977603defc9941b9f9bc
David Abrahams <david.abrahams@students.olin.edu>
2018-02-26 12:27:08 -0500

    Move commands to commands.c

---
+++ include/commands.h
@@ -0,0 +1,8 @@
+
+char* concat(const char *s1, const char *s2);
+
+void ls();
+
+void cd(char *loc);
+
+
--- src/basicBash.c
+++ src/basicBash.c
@@ -5,38 +5,6 @@
 #include <string.h>
 #include "../include/loop.h"
 
-char* concat(const char *s1, const char *s2) {
-  char *result = malloc(strlen(s1)+strlen(s2)+1);
-  strcpy(result, s1);
-  strcat(result, s2);
-  return result;
-}
-
-
-void ls() {
-  DIR *d;
-  struct dirent *dir;
-  d = opendir(".");
-
-  if (d) {
-    while ((dir = readdir(d)) != NULL) {
-      printf("%s\n", dir->d_name);
-    }
-    closedir(d);
-  }
-}
-
-
-void cd(char *loc) {
-  char *curLoc = "./";
-  char *directory = concat(curLoc, loc);
-  int ret;
-  
-  ret = chdir(directory);
-  printf("%d", ret);
-}
-
-
 int main(void) {
   loop();
   return(0);
