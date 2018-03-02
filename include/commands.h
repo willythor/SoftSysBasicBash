c1ff52ddddbb390a3db463dcc8ab74ff3510e0a2
willythor <willem.thorbecke@students.olin.edu>
2018-03-01 23:22:47 -0500

    improved interface and cd function

--- include/commands.h
+++ include/commands.h
@@ -2,6 +2,7 @@
 char* concat(const char *s1, const char *s2);
 
 int ls();
+char* displayDirectoryName(int newLine);
 
 int cd(char *loc);
 
--- src/loop.c
+++ src/loop.c
@@ -1,26 +1,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "../include/loop.h"
 #include "../include/matcher.h"
 #include "../include/input.h"
 #include "../include/commands.h"
-
 #include "../include/error.h"
 
 void loop() {
   char *input;
-  int success = SUCCESS;
+  displayDirectoryName(0);
+  int success;
   while (matcher(input = readLine()) != QUIT) {
+    success = SUCCESS;
     if (matcher(input) == LS) {
-      if (ls() == FAILURE) success = FAILURE;
+      if (ls() == FAILURE) {
+        success = FAILURE;
+      }
+      displayDirectoryName(0);
     } else if (matcher(input) == CD) {
-      if (cd("..") == FAILURE) success = FAILURE;
+      strsep(&input," ");
+      if (cd(strsep(&input," ")) == FAILURE) success = FAILURE;
+      displayDirectoryName(0);
     } else if (matcher(input) != QUIT) success = FAILURE;
 
     // handle invalid input
     if (success == FAILURE) {
       printf("Invalid command: %s\n", input);
+      displayDirectoryName(0);
     }
   }
   printf("QUIT");
