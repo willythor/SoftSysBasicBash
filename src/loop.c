0f2b9fc15a257bb05e771ad21ea657da037cf25e
David Abrahams <david.abrahams@students.olin.edu>
2018-03-01 21:15:07 -0500

    tell user when they input invalid commands

--- src/loop.c
+++ src/loop.c
@@ -6,12 +6,22 @@
 #include "../include/input.h"
 #include "../include/commands.h"
 
+#include "../include/error.h"
+
 void loop() {
   char *input;
+  int success = SUCCESS;
   while (matcher(input = readLine()) != QUIT) {
-    printf("Your entered: %s\n", input);
-    if (matcher(input) == LS) ls();
-    if (matcher(input) == CD) cd("..");
+    if (matcher(input) == LS) {
+      if (ls() == FAILURE) success = FAILURE;
+    } else if (matcher(input) == CD) {
+      if (cd("..") == FAILURE) success = FAILURE;
+    } else if (matcher(input) != QUIT) success = FAILURE;
+
+    // handle invalid input
+    if (success == FAILURE) {
+      printf("Invalid command: %s\n", input);
+    }
   }
   printf("QUIT");
 }
