337fa2d8ac11ad175dd765f30799b7c074df422a
willythor <willem.thorbecke@students.olin.edu>
2018-03-15 21:02:57 -0400

    working cd and ls commands with tab completion

--- src/input.c
+++ src/input.c
@@ -1,24 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
 
 #include "../include/input.h"
+#include "../include/commands.h"
+#include "../include/termios.h"
 
 char *readLine() {
-  int lineCapacity = 64;
+  int lineCapacity = 1000;
   int lineSize = 0;
   char *line = malloc(sizeof(char) * lineCapacity);
   char c;
-  // Loop until c is EOF or a newline char
-  while ((c = getchar()) != EOF && c != '\n') {
-    // Need to leave one char on the end for the terminator
-    if (lineSize == lineCapacity) {
-      lineCapacity *= 2;
-      line = realloc(line, sizeof(char) * lineCapacity);
+
+  c = getch();
+
+  while (c != '\n') {
+    if (c == '\t') {
+      char *newLine = tabComplete(line);
+      for (int i = 0; i < lineSize; i++) {
+        printf("\b \b");
+      }
+      line = newLine;
+      lineSize = strlen(line);
+      printf("%s", line);
     }
-    line[lineSize] = c;
-    lineSize++;
+    //0x7f checks for a backspace
+    else if (c == 0x7f) {
+      if (lineSize > 0) {
+        lineSize--;
+        //deletes the previously inputted character
+        printf("\b \b");
+      }
+    }
+    else {
+      //need to print out current char because I'm not echoing
+      printf("%c",c);
+      // Need to leave one char on the end for the terminator
+      if (lineSize == lineCapacity) {
+       lineCapacity *= 2;
+       line = realloc(line, sizeof(char) * lineCapacity);
+      }
+      line[lineSize] = c;
+      lineSize++;
+    }
+    c = getch();
   }
+  
+  printf("%c",'\n');
   line[lineSize] = '\0';
   return line;
 }
